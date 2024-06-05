8a630a140e0712571fb6d3426b9701dde95307b1
Bautista Marelli <marellibautista@gmail.com>
2024-06-05 16:00:06 -0300

    set leader for each os.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,6 +7,9 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
+local is_linux = wezterm.target_triple:find("linux") ~= nil
+local is_macos = wezterm.target_triple:find("apple") ~= nil
+
 config.color_scheme = "Catppuccin Frappe"
 
 config.font = wezterm.font({
@@ -32,13 +35,22 @@ config.window_close_confirmation = "NeverPrompt"
 config.send_composed_key_when_left_alt_is_pressed = false
 config.send_composed_key_when_right_alt_is_pressed = false
 
-config.leader = { key = " ", mods = "CTRL", timeout_milliseconds = 1000 }
+local mod = "CTRL"
+if is_linux then
+  mod = "CTRL"
+  config.leader = { key = " ", mods = "CTRL", timeout_milliseconds = 1500 }
+end
+
+if is_macos then
+  mod = "OPT"
+  config.leader = { key = " ", mods = "OPT", timeout_milliseconds = 1500 }
+end
 
 config.keys = {
-  neovim.forward_or_default("LeftArrow", "CTRL", util.focus_relative_fn("Left")),
-  neovim.forward_or_default("DownArrow", "CTRL", util.focus_relative_fn("Down")),
-  neovim.forward_or_default("UpArrow", "CTRL", util.focus_relative_fn("Up")),
-  neovim.forward_or_default("RightArrow", "CTRL", util.focus_relative_fn("Right")),
+  neovim.forward_or_default("LeftArrow", mod, util.focus_relative_fn("Left")),
+  neovim.forward_or_default("DownArrow", mod, util.focus_relative_fn("Down")),
+  neovim.forward_or_default("UpArrow", mod, util.focus_relative_fn("Up")),
+  neovim.forward_or_default("RightArrow", mod, util.focus_relative_fn("Right")),
   { key = "+", mods = "LEADER|SHIFT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
   { key = "-", mods = "LEADER",       action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
   { key = "f", mods = "LEADER",       action = wezterm.action.TogglePaneZoomState },
