a45b56eacfb2c506fefdf77e4410f6335ac12176
Bautista Marelli <marellibautista@gmail.com>
2024-07-16 19:27:08 -0300

    fixup! feat: change lualine theme.

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -6,6 +6,10 @@ return {
       options = {
         component_separators = '',
         section_separators = { left = '', right = '' },
+      },
+      sections = {
+        lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
+        lualine_z = { { 'location', separator = { right = '' }, left_padding = 2 } },
       }
     })
   end
