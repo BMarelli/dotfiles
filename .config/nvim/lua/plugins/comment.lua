24605414b905af45f186b22eee49e112ca109cd3
Bautista Marelli <marellibautista@gmail.com>
2024-07-16 19:22:31 -0300

    feat: change lualine theme.

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -2,7 +2,7 @@ return {
   "terrortylor/nvim-comment",
   config = function()
     vim.keymap.set("n", "<leader>cl", ":CommentToggle<CR>", { desc = "toogle comment in current line" })
-    vim.keymap.set("n", "<leader>cb", ":'<,'>CommentToggle<CR>", { desc = "toogle comment in block" })
+    vim.keymap.set("v", "<leader>cb", ":'<,'>CommentToggle<CR>", { desc = "toogle comment in block" })
 
     local comment = require("nvim_comment")
     comment.setup({})
