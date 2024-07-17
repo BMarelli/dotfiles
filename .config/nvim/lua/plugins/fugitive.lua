ba02d5dcf8a59636a4b32066e7f55e832e1a2e6a
Bautista Marelli <marellibautista@gmail.com>
2024-07-17 15:07:08 -0300

    feat: fugitive configuration.

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -1,1 +1,11 @@
-return {}
+return {
+  {
+    "tpope/vim-fugitive",
+    dependencies = { "tpope/vim-rhubarb" },
+
+    keys = {
+      { "<leader>gy", "<cmd>GBrowse!<CR>",           mode = { "n" }, desc = "yank permalink" },
+      { "<leader>gy", "<esc><cmd>'<,'>GBrowse!<CR>", mode = { "v" }, desc = "yank permalink" },
+    }
+  }
+}
