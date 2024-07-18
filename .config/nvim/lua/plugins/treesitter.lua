1e94960a22e4a407433069b228a5a1c3dba743cf
Bautista Marelli <marellibautista@gmail.com>
2024-07-18 12:27:56 -0300

    fix: remove `http` and `rest` configuration.

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { "c", "lua", "python", "haskell", "http", "tsx", "html", "css", "yaml", "json" },
+      ensure_installed = { "c", "lua", "python", "haskell", "tsx", "html", "css", "yaml", "json" },
       sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },
