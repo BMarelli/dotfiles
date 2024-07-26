2c215800c74e2190199dce907519231b535766cc
Bautista Marelli <marellibautista@gmail.com>
2024-07-26 11:22:29 -0300

    refactor: change config (dont remember what gg).

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,51 +1,47 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.6",
-    dependencies = {
-      { "nvim-lua/plenary.nvim" },
-      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    },
-    config = function()
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "find files" })
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "live grep" })
-      vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "find recent files" })
-      vim.keymap.set("n", "<leader>fc", builtin.grep_string, { desc = "find string in file" })
-    end,
-
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    { "nvim-lua/plenary.nvim" },
   },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-      require("telescope").setup {
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown {}
-          }
-        },
 
-        defaults = {
-          layout_config = {
-            prompt_position = "top",
+  cmd = { "Telescope" },
 
-            horizontal = { preview_width = 0.65 },
-            width = { padding = 0 },
-            height = { padding = 0 },
-            preview_width = 0.65,
-          },
+  keys = {
+    {
+      "<leader>ff",
+      "<cmd>Telescope find_files<CR>",
+      desc = "find files",
+    },
+    { "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "buffers" },
+    { "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "live grep" },
+    { "<leader>sh", "<cmd>Telescope help_tags<CR>", desc = "help tags" },
+  },
 
-          sorting_strategy = "ascending",
-        },
+  opts = {
+    defaults = {
+      path_display = { "smart" },
 
-        pickers = {
-          git_files = {
-            show_untracked = true,
-          },
-        },
-      }
-      require("telescope").load_extension("ui-select")
-      require("telescope").load_extension("fzf")
-    end
-  }
+      layout_config = {
+        prompt_position = "top",
+        horizontal = { preview_width = 0.65 },
+        width = { padding = 0 },
+        height = { padding = 0 },
+      },
+
+      sorting_strategy = "ascending",
+    },
+
+    pickers = {
+      git_files = {
+        show_untracked = true,
+      },
+    },
+  },
+
+  config = function(_, opts)
+    require("telescope").setup(opts)
+
+    require("telescope").load_extension("fzf")
+  end,
 }
