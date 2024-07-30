6dfb776afedf9f6c56ca434bcd86f65c612a2c5f
Bautista Marelli <marellibautista@gmail.com>
2024-07-29 23:12:50 -0300

    feat: markdown render.

--- .config/nvim/lua/plugins/lang/markdown.lua
+++ .config/nvim/lua/plugins/lang/markdown.lua
@@ -29,4 +29,11 @@ return {
       },
     },
   },
+  {
+    "MeanderingProgrammer/markdown.nvim",
+    main = "render-markdown",
+    opts = {},
+    name = "render-markdown",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+  },
 }
