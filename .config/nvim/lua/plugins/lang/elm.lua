09267794a2b2ef036bed6188a4226e6d91fc58f5
Bautista Marelli <marellibautista@gmail.com>
2024-07-30 22:47:48 -0300

    feat: add elm configuration.

---
+++ .config/nvim/lua/plugins/lang/elm.lua
@@ -0,0 +1,37 @@
+return {
+  recommended = {
+    ft = { "elm" },
+    root = { "elm.json" },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "elm" } },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = { ensure_installed = { "elm-format" } },
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        elm = { "elm_format" },
+      },
+      formatters = {
+        elm_format = {
+          command = "elm-format",
+          args = { "--stdin" },
+        },
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        elmls = {},
+      },
+    },
+  },
+}
