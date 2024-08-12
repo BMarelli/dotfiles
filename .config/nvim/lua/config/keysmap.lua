d13605680c2114c1afaa198a94940be2e1382962
Bautista Marelli <marellibautista@gmail.com>
2024-08-12 10:24:30 -0300

    fix: split window keysmap

--- .config/nvim/lua/config/keysmap.lua
+++ .config/nvim/lua/config/keysmap.lua
@@ -5,8 +5,8 @@ vim.keymap.set("n", "<leader>s<Up>", "<C-w>k", { desc = "move to the split above
 vim.keymap.set("n", "<leader>s<Down>", "<C-w>j", { desc = "move to the split below" })
 vim.keymap.set("n", "<leader>s<Left>", "<C-w>h", { desc = "move to the split on the left" })
 vim.keymap.set("n", "<leader>s<Right>", "<C-w>l", { desc = "move to the split on the right" })
-vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "split window vertically" })
-vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "split window horizontally" })
+vim.keymap.set("n", "<leader>s+", "<C-w>v", { desc = "split window vertically" })
+vim.keymap.set("n", "<leader>s-", "<C-w>s", { desc = "split window horizontally" })
 vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "close current split" })
 
 vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "creates a new tab" })
